9be4230dea091fbaeed3868e7f6122a6f27cc6ec
Lorenzo Baldassari <lorebalda93@gmail.com>
2023-12-18 14:51:39 +0100

    class Created

---
+++ src/main/java/Main1/entites/Tram.java
@@ -0,0 +1,39 @@
+package Main1.entites;
+
+import Main1.entites.Enum.Stato;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Tram extends Parco_mezzi{
+
+    protected long capienza;
+
+    public Tram(Stato stato, List<LocalDate> giorniDiServizio, List<LocalDate> giorniDiManutenzione) {
+        super(stato, giorniDiServizio, giorniDiManutenzione);
+        this.capienza = 50;
+    }
+
+    public Tram(Stato stato) {
+        super(stato);
+        this.capienza = 50;
+    }
+
+    public long getCapienza() {
+        return capienza;
+    }
+
+    public Tram() {
+    }
+
+    @Override
+    public String toString() {
+        return "Tram{" +
+                "capienza=" + capienza +
+                ", id=" + id +
+                ", stato=" + stato +
+                ", giorniDiServizio=" + giorniDiServizio +
+                ", giorniDiManutenzione=" + giorniDiManutenzione +
+                '}';
+    }
+}
